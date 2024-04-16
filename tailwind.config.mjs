39d963351370842902ebbb4376789b9bd493e858
edu lenovo <eduardochamacoelguapo@gmail.com>
2024-04-16 11:37:07 +0100

    animacion a logos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,17 @@ export default {
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "loop-scroll": "loop-scroll 50s linear infinite",
+      },
+      keyframes: {
+        "loop-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
